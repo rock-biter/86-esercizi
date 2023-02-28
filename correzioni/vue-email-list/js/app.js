71a1be8f10171c6ef574b1159696352d26969a44
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-02-28 10:03:45 +0100

    correzione vue-email-list

---
+++ correzioni/vue-email-list/js/app.js
@@ -0,0 +1,30 @@
+const { createApp } = Vue
+
+createApp({
+	data() {
+		return {
+			emailList: [],
+		}
+	},
+	methods: {
+		fetchEmail() {
+			axios
+				.get('https://flynn.boolean.careers/exercises/api/random/mail')
+				.then((res) => {
+					console.log(res, res.data)
+					const { response: email } = res.data
+					// const email = res.data.response
+					console.log(email)
+					this.emailList.push(email)
+				})
+		},
+		fetchEmailList(num) {
+			for (let i = 0; i < num; i++) {
+				this.fetchEmail()
+			}
+		},
+	},
+	mounted() {
+		this.fetchEmailList(100)
+	},
+}).mount('#app')
