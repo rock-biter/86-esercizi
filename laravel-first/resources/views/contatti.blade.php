a16e1f14746047487123de7f1da1226c271414dc
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-04-04 13:29:20 +0200

    Lezione 625 - laravel primi passi

---
+++ laravel-first/resources/views/contatti.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  <h1>Ciao Mondo!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quasi eaque deleniti corrupti dolore impedit eveniet, unde, possimus praesentium quaerat molestias saepe sapiente libero accusamus accusantium voluptatibus iste. Incidunt, numquam.</p>
+  
+</body>
+</html>
