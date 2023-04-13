43451646c2ff1673d92caa70f9a4bf8d16cb1e74
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-04-13 13:02:30 +0200

    ADD - paragraph to train index view

--- laravel-migration/resources/views/trains/index.blade.php
+++ laravel-migration/resources/views/trains/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
       <h1>Elenco dei treni in partenza</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa minus fugit earum molestiae eos repellendus incidunt doloremque! Nisi illo ipsa culpa? Iusto magni corrupti maxime alias quia quidem accusantium fugiat.</p>
     </div>
 
     <div class="container">
